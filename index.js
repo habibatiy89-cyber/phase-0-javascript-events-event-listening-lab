fc60e72788a9efd246aeaf7254f028fbbe1551e4
habiba hussein <habiba.hussein@moringaschool.comclear>
2025-10-01 05:02:53 +0300

    Add click event listener to button using addEventListener

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
 }
+function addingEventListener() {
+  // Grab the button element
+  const input = document.getElementById('button');
+
+  // Define the callback function
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+
+  // Add the click event listener
+  input.addEventListener('click', clickAlert);
+}
+
+// Call the outer function to set up the event listener
+addingEventListener();
